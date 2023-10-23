3feb5d9ee627b59ac3dc87455c969e2148b79bda
Dima Unychenko <dima@mail.rui>
2023-10-23 21:31:04 +0200

    integration

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Форма для заявок</title>
+</head>
+<body>
+<h1>Заполните форму для заявки</h1>
+<form action="process_form.php" method="post">
+    <label for="name">Имя:</label>
+    <input type="text" id="name" name="name" required>
+    <br>
+
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required>
+    <br>
+
+    <label for="phone">Телефон:</label>
+    <input type="tel" id="phone" name="phone" required>
+    <br>
+
+    <label for="price">Цена:</label>
+    <input type="number" id="price" name="price">
+    <br>
+
+    <button type="submit">Отправить заявку</button>
+</form>
+</body>
+</html>
